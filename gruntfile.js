7b70e89c22cb44b6b59a4d228bfa5c77f19d6c47
George Crawford <george.crawford@ft.com>
2015-11-24 14:08:55 +0000

    Fix grunt task name

--- gruntfile.js
+++ gruntfile.js
@@ -59,7 +59,7 @@ module.exports = function (grunt) {
 
     require('load-grunt-tasks')(grunt)
     grunt.registerTask('default', ['eslint', 'build', 'mocha_istanbul'])
-    grunt.registerTask('build', 'Build wdio-dot-reporter', function () {
+    grunt.registerTask('build', 'Build wdio-junit-reporter', function () {
         grunt.task.run([
             'clean',
             'babel'
